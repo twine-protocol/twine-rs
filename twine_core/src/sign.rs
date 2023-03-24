86c1258e3040cbb6365cafd2b8c305381b5b329c
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2023-03-24 10:26:05 -0400

    Add jose dep + framework signing

--- twine_core/src/sign.rs
+++ twine_core/src/sign.rs
@@ -1,4 +1,14 @@
 
 pub struct Signer {
+    // ...
+}
 
+#[derive(Debug, Clone)]
+pub struct SignerError;
+
+impl Signer {
+    fn from_keys(public_key: &[u8], private_key: &[u8]) -> Result<Signer, SignerError> {}
+    fn from_random() -> Result<Signer, SignerError> {}
+    fn sign(&self, data: &[u8]) -> Result<Vec<u8>, SignerError> {}
+    fn verify(&self, data: &[u8], signature: &[u8]) -> Result<bool, SignerError> {}
 }
