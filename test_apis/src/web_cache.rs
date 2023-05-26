23df452abf101622ae54985b562dabbf4e636d42
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2023-05-26 16:08:07 -0400

    Testable web cache

--- test_apis/src/web_cache.rs
+++ test_apis/src/web_cache.rs
@@ -1,16 +1,19 @@
 use std::collections::HashMap;
-use std::sync::{Mutex, PoisonError, MutexGuard};
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Mutex;
 
 use rocket::State;
 use rocket::response::Responder;
-use rocket::response::content::{RawHtml, RawJson};
+use rocket::response::content::{RawJson};
 use rocket::fairing::AdHoc;
 use rocket::{get, routes};
 use thiserror::Error;
-use twine_core::libipld::Cid;
+use twine_builder::{PulseBuilder, ChainBuilder};
+use twine_core::josekit::jwk::alg::ed::EdCurve::Ed25519;
+use twine_core::josekit::jws::alg::eddsa::EddsaJwsAlgorithm;
+use twine_core::libipld::{Cid, ipld, multihash};
 use twine_core::twine::{Pulse, Chain};
 use crate::helpers::ParamCid;
+use crate::map;
 use twine_core::twine::Twine;
 
 type DangerousChainCache = HashMap<Cid, Chain>;
@@ -45,9 +48,30 @@ fn index(chain_cid: ParamCid, pulse_cid: ParamCid, cache: &State<PulseCache>) ->
 }
 
 pub fn stage() -> AdHoc {
-    AdHoc::on_ignite("Managed Hit Count", |rocket| async {
+    AdHoc::on_ignite("Chain/Pulse", |rocket| async {
+        let keys = EddsaJwsAlgorithm::Eddsa.generate_key_pair(Ed25519).expect("Can make keys");
+        let signer = EddsaJwsAlgorithm::Eddsa.signer_from_jwk(&keys.to_jwk_private_key()).expect("Can make signer");
+        let verifier = EddsaJwsAlgorithm::Eddsa.verifier_from_jwk(&keys.to_jwk_public_key()).expect("Can make verifier");
+        let hasher = multihash::Code::Sha3_512;
+
+        let chain = ChainBuilder::new(
+            "gold".into(),
+            HashMap::new(),
+            keys.to_jwk_public_key()
+        )
+        .finalize( &signer, &verifier, hasher)
+        .expect("Should be able to make chains");
+        
+        let pulse = PulseBuilder::first(&chain)
+            .payload(map!{ String::from("Hello") => ipld!{ "world" } })
+            .finalize(&signer, &verifier)
+            .expect("Should be able to make pulses");
+        
+        println!("chain {:?} : pulse {:?}", chain.cid, pulse.cid)
+
+        let state = PulseCache::new(map!{ chain.cid => map!{ pulse.cid => pulse } });
         rocket.mount("/", routes![index])
-            .manage(PulseCache::new(HashMap::new()))
+            .manage(state)
     })
 }
 
