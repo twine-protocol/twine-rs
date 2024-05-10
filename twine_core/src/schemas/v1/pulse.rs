4a95f4f87591be8955bd71a913db0f691a017513
Jasper <well.caffeinated@gmail.com>
2024-05-09 18:43:10 -0600

    pulsecontent with payload type

--- twine_core/src/schemas/v1/pulse.rs
+++ twine_core/src/schemas/v1/pulse.rs
@@ -5,13 +5,13 @@ use super::Mixin;
 use crate::verify::is_all_unique;
 
 #[derive(Debug, Serialize, Clone, Deserialize, PartialEq)]
-pub struct PulseContentV1 {
+pub struct PulseContentV1<P = Ipld>{
   pub chain: Cid,
   pub index: u32, // note: DAG-CBOR supports i64, but we don't
   pub source: String,
   pub links: Vec<Cid>,
   pub mixins: Vec<Mixin>, // we check that these links are not on the same chain at runtime
-  pub payload: Ipld,
+  pub payload: P,
 }
 
 impl Verifiable for PulseContentV1 {
