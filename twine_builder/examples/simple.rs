de2a6e12a8f3b20c868937acbe0e0c3b93e4c805
Jasper <well.caffeinated@gmail.com>
2024-05-09 18:42:45 -0600

    builder implemented

---
+++ twine_builder/examples/simple.rs
@@ -0,0 +1,46 @@
+use twine_builder::Builder;
+use twine_core::libipld::ipld;
+use josekit::jwk;
+
+fn main() {
+  let signer = jwk::Jwk::generate_ed_key(jwk::alg::ed::EdCurve::Ed25519).unwrap();
+  let builder = Builder::new(signer);
+  let strand = builder.build_strand()
+    .version("1.0.0".to_string())
+    .details(ipld!({
+      "foo": "bar",
+    }))
+    .done()
+    .unwrap();
+
+  let mut signatures = vec![];
+  let mut prev = builder.build_first(strand.clone())
+    .payload(ipld!({
+      "baz": "qux",
+    }))
+    .done()
+    .unwrap();
+
+  signatures.push(prev.signature().to_string());
+
+  let n = 1000;
+  let start_time = std::time::Instant::now();
+  for i in 1..n {
+    prev = builder.build_next(prev)
+      .payload(ipld!({
+        "baz": "qux",
+        "index": i,
+      }))
+      .done()
+      .unwrap();
+
+    signatures.push(prev.signature().to_string());
+  }
+
+  let elapsed = start_time.elapsed();
+  let twines_per_second = n as f64 / elapsed.as_secs_f64();
+  println!(
+    "Built {} twines in {:.2} seconds ({:.2} twines per second, {:.2} microsec per twine)",
+    n, elapsed.as_secs_f64(), twines_per_second, elapsed.as_micros() as f64 / n as f64
+  );
+}
--- twine_builder/src/builder.rs
+++ twine_builder/src/builder.rs
@@ -1,5 +1,7 @@
+use std::sync::Arc;
+
 use twine_core::{
-  errors::{SpecificationError, VerificationError}, libipld::multihash::{Code, MultihashDigest}, semver::Version, specification::Subspec, twine::{container::TwineContent, CrossStitches, Strand, StrandContent}, verify::Verified, Ipld
+  errors::{SpecificationError, VerificationError}, libipld::multihash::{Code, MultihashDigest}, semver::Version, skiplist::get_layer_pos, specification::Subspec, twine::{container::TwineContent, CrossStitches, Stitch, Strand, StrandContent, Tixel, TixelContent, Twine}, verify::Verified, Ipld
 };
 use crate::{signer::SigningError, Signer};
 
@@ -11,6 +13,8 @@ pub enum BuildError {
   BadSpecification(#[from] SpecificationError),
   #[error("Problem signing: {0}")]
   ProblemSigning(#[from] SigningError),
+  #[error("Tixel index maximum reached")]
+  IndexMaximum,
 }
 
 pub struct Builder<S: Signer> {
@@ -26,6 +30,129 @@ impl <S: Signer> Builder<S> {
 
   pub fn build_strand<'a>(&'a self) -> StrandBuilder<'a, S> {
     StrandBuilder::new(&self.signer)
+  }
+
+  pub fn build_first<'a>(&'a self, strand: Strand) -> TixelBuilder<'a, S> {
+    TixelBuilder::new_first(&self.signer, Arc::new(strand))
+  }
+
+  pub fn build_next<'a>(&'a self, prev: Twine) -> TixelBuilder<'a, S> {
+    TixelBuilder::new_next(&self.signer, prev)
+  }
+}
+
+pub struct TixelBuilder<'a, S: Signer> {
+  signer: &'a S,
+  strand: Arc<Strand>,
+  prev: Option<Twine>,
+  stitches: CrossStitches,
+  payload: Ipld,
+  source: String,
+}
+
+impl <'a, S: Signer> TixelBuilder<'a, S> {
+  pub fn new_first(signer: &'a S, strand: Arc<Strand>) -> Self {
+    Self {
+      signer,
+      strand,
+      prev: None,
+      stitches: CrossStitches::default(),
+      payload: Ipld::Null,
+      source: String::new(),
+    }
+  }
+
+  pub fn new_next(signer: &'a S, prev: Twine) -> Self {
+    Self {
+      signer,
+      strand: prev.strand(),
+      prev: Some(prev),
+      stitches: CrossStitches::default(),
+      payload: Ipld::Null,
+      source: String::new(),
+    }
+  }
+
+  pub fn cross_stitches<C: Into<CrossStitches>>(mut self, stitches: C) -> Self {
+    self.stitches = stitches.into();
+    self
+  }
+
+  pub fn payload(mut self, payload: Ipld) -> Self {
+    self.payload = payload;
+    self
+  }
+
+  #[deprecated(note = "Use payload() or strand.details() instead")]
+  pub fn source(mut self, source: String) -> Self {
+    self.source = source;
+    self
+  }
+
+  fn next_back_stitches(&self) -> Result<Vec<Stitch>, BuildError> {
+    if let Some(prev) = &self.prev {
+      let mut stitches = prev.back_stitches();
+      let radix = self.strand.radix();
+      let pindex = prev.index();
+      if pindex == 0 {
+        return Ok(vec![prev.clone().into()]);
+      }
+
+      let expected_len = if radix == 0 {
+        1
+      } else {
+        ((pindex as f64).log(radix as f64).ceil()).max(1.) as usize
+      };
+      if stitches.len() != expected_len {
+        // (`Previous links array has incorrect size. Expected: ${expected_len}, got: ${links.length}`)
+        return Err(BuildError::BadData(VerificationError::InvalidTwineFormat(format!(
+          "Previous links array has incorrect size. Expected: {}, got: {}",
+          expected_len, stitches.len()
+        ))));
+      }
+
+      if radix == 0 {
+        return Ok(vec![prev.clone().into()]);
+      }
+
+      let z = get_layer_pos(radix, pindex) + 1;
+      if z > stitches.len() {
+        stitches.resize(z, prev.clone().into());
+      }
+
+      stitches.splice(0..z, std::iter::repeat(prev.clone().into()).take(z));
+      Ok(stitches)
+    } else {
+      Ok(vec![])
+    }
+  }
+
+  pub fn done(self) -> Result<Twine, BuildError> {
+    use twine_core::schemas::*;
+    let content: TixelContent = match self.strand.version().major {
+      1 => v1::PulseContentV1 {
+        index: self.prev.as_ref().map(|p|
+          (p.index() as u32).checked_add(1)
+            .ok_or(BuildError::IndexMaximum)
+        ).unwrap_or(Ok(0))?,
+        links: self.next_back_stitches()?.into_iter().map(|s| s.tixel).collect(),
+        payload: self.payload,
+        mixins: self.stitches.stitches().into_iter().collect(),
+        chain: self.strand.cid(),
+        source: self.source,
+      }.into(),
+      _ => return Err(BuildError::BadSpecification(
+        SpecificationError::new(format!("Unsupported version: {}", self.strand.version()))
+      )),
+    };
+
+    let hasher = self.strand.hasher();
+    let bytes = content.bytes();
+    let dat = hasher.digest(&bytes).to_bytes();
+    let signature = self.signer.sign(&dat)?;
+
+    let tixel = Tixel::new_from_parts(hasher, Verified::try_new(content)?, signature);
+    Ok(Twine::try_new_from_shared(self.strand, Arc::new(tixel))?)
   }
 }
 
@@ -212,5 +339,43 @@ mod test {
 
     assert!(strand.is_ok(), "{}", strand.unwrap_err());
     assert!(strand.unwrap().verify_own_signature().is_ok(), "Failed to verify signature");
+  }
+
+  #[test]
+  fn text_build_tixels() {
+    let signer = jwk::Jwk::generate_ed_key(jwk::alg::ed::EdCurve::Ed25519).unwrap();
+    let builder = Builder::new(signer);
+    let strand = builder.build_strand()
+      .version("1.0.0".to_string())
+      .details(ipld!({
+        "foo": "bar",
+      }))
+      .radix(2)
+      .done()
+      .unwrap();
+
+    let mut signatures = vec![];
+    let mut prev = builder.build_first(strand.clone())
+      .payload(ipld!({
+        "baz": "qux",
+      }))
+      .done()
+      .unwrap();
+
+    signatures.push(prev.signature().to_string());
+
+    for i in 1..10 {
+      prev = builder.build_next(prev)
+        .payload(ipld!({
+          "baz": "qux",
+          "index": i,
+        }))
+        .done()
+        .unwrap();
+
+      signatures.push(prev.signature().to_string());
+    }
+
+    dbg!(signatures);
   }
 }
