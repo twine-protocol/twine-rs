762e6bf6ea4e59c1df35d879691d6f3d38ea28b6
Jasper Palfree <well.caffeinated@gmail.com>
2024-12-18 17:14:50 -0700

    add from_car_bytes helper function

--- twine_core/src/car.rs
+++ twine_core/src/car.rs
@@ -1,10 +1,20 @@
+use std::io::Read;
 use futures::Stream;
 use futures::stream::StreamExt;
-use crate::Cid;
+use rs_car_sync::CarReader;
+use crate::{errors::VerificationError, twine::AnyTwine, Cid};
 use ipld_core::codec::Codec;
 use serde_ipld_dagcbor::codec::DagCborCodec;
 use serde::{Deserialize, Serialize};
 use crate::twine::TwineBlock;
+
+#[derive(Debug, thiserror::Error)]
+pub enum CarDecodeError {
+  #[error("{0}")]
+  VerificationError(#[from] VerificationError),
+  #[error("Error decoding CAR: {0}")]
+  DecodeError(#[from] rs_car_sync::CarDecodeError),
+}
 
 // Max size of u64 varint
 const U64_LEN: usize = 10;
@@ -58,6 +68,18 @@ pub fn to_car_stream<I: TwineBlock, S: Stream<Item=I>>(stream: S, roots: Vec<Cid
     .chain(blocks)
 }
 
+pub fn from_car_bytes<R: Read>(mut reader: &mut R) -> Result<Vec<AnyTwine>, CarDecodeError> {
+  // block validation happens in twine creation
+  let car_reader = CarReader::new(&mut reader, false)?;
+  car_reader.map(|result| -> Result<AnyTwine, CarDecodeError> {
+    let (cid, bytes) = result?;
+    let cid = Cid::read_bytes(&*cid.to_bytes()).expect("cid should be valid format");
+    let twine = AnyTwine::from_block(cid, bytes)?;
+    Ok(twine)
+  })
+  .collect()
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -82,6 +104,20 @@ mod test {
     let (cid, bytes) = reader.next().await.unwrap().unwrap();
     assert_eq!(cid.to_bytes(), twine.cid().to_bytes());
     assert_eq!(*bytes, *twine.bytes());
+    Ok(())
+  }
+
+  #[tokio::test]
+  async fn test_from_car_bytes() -> Result<(), Box<dyn Error>> {
+    let twine = Strand::from_tagged_dag_json(STRANDJSON).unwrap();
+    let roots = vec![twine.cid()];
+    let stream = futures::stream::iter(vec![twine.clone()]);
+    let car_stream = to_car_stream(stream, roots.clone());
+    let car_bytes = car_stream.collect::<Vec<_>>().await.concat();
+    let twines = from_car_bytes(&mut &*car_bytes).unwrap();
+    assert_eq!(twines.len(), 1);
+    assert_eq!(twines[0].cid(), twine.cid());
+    assert_eq!(twines[0].bytes(), twine.bytes());
     Ok(())
   }
 }
