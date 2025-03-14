9174acfdd16d0ec56d129b7e12bc6cbf8a963843
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-14 14:06:56 -0600

    update(docs): document serde module

--- twine_lib/src/serde.rs
+++ twine_lib/src/serde.rs
@@ -1,9 +1,26 @@
 use serde::{Deserialize, Serialize, Serializer};
 
 /// For use with serde_with to serialize and deserialize IPLD DAG-JSON
+///
+/// Mainly useful when using some framework that deserializes using
+/// serde_json, but you want it to use dag_json.
+///
+/// # Example
+///
+/// ```rust
+/// use serde::{Deserialize, Serialize};
+/// use twine_lib::Ipld;
+///
+/// #[derive(Debug, Serialize, Deserialize)]
+/// struct MyStruct {
+///   #[serde(with = "twine_lib::serde::dag_json")]
+///   some_obj: Ipld,
+/// }
+/// ```
 pub mod dag_json {
   use super::*;
 
+  #[allow(missing_docs)]
   pub fn serialize<S: Serializer, T: Serialize>(
     value: &T,
     serializer: S,
@@ -12,6 +29,7 @@ pub mod dag_json {
     value.serialize(ser)
   }
 
+  #[allow(missing_docs)]
   pub fn deserialize<'de, D: serde::Deserializer<'de>, T: Deserialize<'de>>(
     deserializer: D,
   ) -> std::result::Result<T, D::Error> {
