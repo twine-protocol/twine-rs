d2b49a1770c85ce3c8a133967279c43ac96f7d5d
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-15 12:46:29 -0600

    update(docs): document the Twine struct

--- twine_lib/src/twine/twine.rs
+++ twine_lib/src/twine/twine.rs
@@ -9,6 +9,14 @@ use crate::{
   twine::{Strand, Tixel},
 };
 
+/// Primary data structure for interacting with Twine records
+///
+/// A Twine struct is a Tixel/Strand pair. Any Twine created
+/// will automatically have verified the Tixel and Strand integrity
+/// and authenticity.
+///
+/// Since Strands and Tixels are effectively Arcs of their underlying
+/// data, Twines are efficient to clone and pass around.
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Twine {
   strand: Strand,
@@ -16,23 +24,31 @@ pub struct Twine {
 }
 
 impl Twine {
+  /// Create a new Twine from a Strand and Tixel
   pub fn try_new(strand: Strand, tixel: Tixel) -> Result<Self, VerificationError> {
     strand.verify_tixel(&tixel)?;
     Ok(Self { strand, tixel })
   }
 
+  /// Get a reference to the Strand
   pub fn strand(&self) -> &Strand {
     &self.strand
   }
 
+  /// Get a reference to the Tixel
   pub fn tixel(&self) -> &Tixel {
     &self.tixel
   }
 
+  /// Get the radix value of the Strand
   pub fn radix(&self) -> u8 {
     self.strand().radix()
   }
 
+  /// Get the twine version of this record
+  ///
+  /// In Twine v1, the version is determined by the Strand.
+  /// In Twine v2, the version is determined by the Tixel.
   pub fn version(&self) -> Version {
     let strand_ver = self.strand().version();
     match strand_ver.major {
@@ -41,6 +57,7 @@ impl Twine {
     }
   }
 
+  /// Get the subspec of this record if it exists
   pub fn subspec(&self) -> Option<Subspec> {
     let strand_ver = self.strand().version();
     match strand_ver.major {
