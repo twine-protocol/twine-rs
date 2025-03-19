d331d17efe20bc26ade26e374deed43fb1eb0e09
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-19 11:59:41 -0600

    update(docs): document Strand

--- twine_lib/src/twine/strand.rs
+++ twine_lib/src/twine/strand.rs
@@ -16,10 +16,26 @@ use serde_ipld_dagcbor::codec::DagCborCodec;
 use serde_ipld_dagjson::codec::DagJsonCodec;
 use std::{fmt::Display, sync::Arc};
 
+/// A Strand represents the metadata for the chain of Tixels
+///
+/// A Strand is verified for integrity and authenticity via
+/// the CID and public key it contains.
+///
+/// The most common way to obtain a Strand will be from a [`crate::twine::Twine`],
+/// a [`crate::resolver::Resolver`], or using [`Strand::from_tagged_dag_json`]
+/// or [`Strand::from_block`].
+///
+/// # See also
+///
+/// - [`Tixel`]
+/// - [`crate::twine::Twine`]
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Strand(pub(crate) Arc<Verified<StrandSchemaVersion>>);
 
 impl Strand {
+  /// Create a new Strand from its underlying data
+  ///
+  /// It is uncommon to use this directly.
   pub fn try_new<C>(container: C) -> Result<Self, VerificationError>
   where
     C: TryInto<StrandSchemaVersion>,
@@ -29,47 +45,61 @@ impl Strand {
     Ok(Self(Arc::new(Verified::try_new(container)?)))
   }
 
+  /// Get the CID of the Strand
   pub fn cid(&self) -> Cid {
     *self.0.cid()
   }
 
+  /// Get the public key of the Strand
   pub fn key(&self) -> PublicKey {
     self.0.key()
   }
 
+  /// Get the radix value of the skiplist
   pub fn radix(&self) -> u8 {
     self.0.radix()
   }
 
+  /// Get the spec string of the Strand
   pub fn spec_str(&self) -> &str {
     self.0.spec_str()
   }
 
+  /// Get the twine version of this record
   pub fn version(&self) -> Version {
     self.0.version()
   }
 
+  /// Get the subspec of this record if it exists
   pub fn subspec(&self) -> Option<Subspec> {
     self.0.subspec()
   }
 
+  /// Get the details of the Strand
+  ///
+  /// Details describe the Strand in a human-readable way
+  /// and can contain any data, usually described by the subspec.
   pub fn details(&self) -> &Ipld {
     self.0.details()
   }
 
+  /// Deserialize the details as a specific type
   pub fn extract_details<T: DeserializeOwned>(&self) -> Result<T, VerificationError> {
     let details = self.details();
     Ok(from_ipld(details.clone()).map_err(|e| VerificationError::Payload(e.to_string()))?)
   }
 
+  /// Get the expiry date of the Strand
   pub fn expiry(&self) -> Option<chrono::DateTime<chrono::Utc>> {
     self.0.expiry()
   }
 
+  /// Verify a Tixel using this Strand's public key
   pub fn verify_tixel(&self, tixel: &Tixel) -> Result<(), VerificationError> {
     self.0.verify_tixel(tixel)
   }
 
+  /// Get the hasher ([`Code`]) used to compute the CID
   pub fn hasher(&self) -> Code {
     self.0.hasher()
   }
