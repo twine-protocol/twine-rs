aaebde2de94b0ca9ad40482c37b507829b703758
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-19 16:49:31 -0600

    update(docs): document hashing utilities

--- twine_lib/src/crypto/hash.rs
+++ twine_lib/src/crypto/hash.rs
@@ -3,6 +3,7 @@ use crate::Cid;
 use ipld_core::codec::Codec;
 use multihash_codetable::{Code, MultihashDigest};
 
+/// Get the hash function ([`Code`]) used by a CID
 pub fn get_hasher(cid: &Cid) -> Result<Code, VerificationError> {
   cid
     .hash()
@@ -11,12 +12,14 @@ pub fn get_hasher(cid: &Cid) -> Result<Code, VerificationError> {
     .map_err(|_| VerificationError::UnsupportedHashAlgorithm)
 }
 
+/// Compute the CID of some data using a given hash function
 pub fn get_cid<D: AsRef<[u8]>>(hasher: Code, dat: D) -> Cid {
   let mh = hasher.digest(dat.as_ref());
   let code = <serde_ipld_dagcbor::codec::DagCborCodec as Codec<bool>>::CODE;
   Cid::new_v1(code, mh)
 }
 
+/// Assert that two CIDs are equal, if they are not, return a [`VerificationError`]
 pub fn assert_cid(expected: &Cid, actual: &Cid) -> Result<(), VerificationError> {
   if expected != actual {
     return Err(VerificationError::CidMismatch {
