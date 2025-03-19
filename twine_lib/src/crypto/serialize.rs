12692681c3a5d160c51ba56d0ed87d8b86f4a954
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-19 16:55:41 -0600

    update(docs): document crypto_serialize

--- twine_lib/src/crypto/serialize.rs
+++ twine_lib/src/crypto/serialize.rs
@@ -2,6 +2,7 @@ use serde::Serialize;
 use serde_ipld_dagcbor::EncodeError;
 use std::collections::TryReserveError;
 
+/// Serialize a type to a CBOR-encoded byte vector
 pub fn crypto_serialize<S: Serialize>(input: S) -> Result<Vec<u8>, EncodeError<TryReserveError>> {
   let bytes = serde_ipld_dagcbor::to_vec(&input)?;
   Ok(bytes)
