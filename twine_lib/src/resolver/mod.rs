d3d72a23ac6c1a6d68c0f035ea72251d3c9da5dc
Jasper Palfree <well.caffeinated@gmail.com>
2025-03-20 16:15:03 -0600

    update(docs): document resolver module

--- twine_lib/src/resolver/mod.rs
+++ twine_lib/src/resolver/mod.rs
@@ -13,11 +13,16 @@ pub use query::*;
 mod resolution;
 pub use resolution::*;
 
+/// A module containing the [`BaseResolver`] trait that is
+/// meant to be implemented by any type that wants to be
+/// used as a Twine Resolver.
 pub mod unchecked_base;
 use unchecked_base::*;
 
+/// Optional Send trait which is not Send on wasm32
 #[cfg(target_arch = "wasm32")]
 pub trait MaybeSend {}
+/// Optional Send trait which is not Send on wasm32
 #[cfg(not(target_arch = "wasm32"))]
 pub trait MaybeSend: Send {}
 
@@ -27,9 +32,62 @@ impl<T> MaybeSend for T where T: {}
 #[cfg(not(target_arch = "wasm32"))]
 impl<T> MaybeSend for T where T: Send {}
 
+/// This is a standardized interface for retrieving Twine objects
+///
+/// Datastores will implement this trait (indirectly through [`BaseResolver`])
+/// to provide a consistent interface for retrieving Twine objects.
+///
+/// By retrieving Twine objects through this interface, the caller can
+/// be sure that the data is consistent and verified for integrity and authenticity.
+///
+/// The methods of this trait accept arguments that implement the [`Into`] trait
+/// or similar. This allows for a variety of ways of using the methods
+/// making them more accessible and understandable.
+///
+/// # Example
+///
+/// ```no_run
+/// # use twine_lib::{resolver::{Resolver, SingleQuery}, errors::ResolutionError, Cid};
+/// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+/// use twine_lib::store::MemoryStore;
+/// let some_resolver = MemoryStore::default();
+/// let some_strand_cid: Cid = "bafyrmieej3j3sprtnbfziv6vhixzr3xxrcabnma43ajb5grhsixdvxzdvu".parse().unwrap();
+/// // resolve the latest on the Strand
+/// let latest = some_resolver.resolve_latest(some_strand_cid).await?;
+/// // or do the same but use a Strand object
+/// let strand = some_resolver.resolve_strand(some_strand_cid).await?.unpack();
+/// let latest = some_resolver.resolve_latest(&strand).await?;
+/// // resolve the 31st Tixel on the Strand
+/// let thirty_first = some_resolver.resolve_index(some_strand_cid, 31).await?;
+/// // or use an argument that implements Into<Query> like a tuple
+/// let thirty_first = some_resolver.resolve((some_strand_cid, 31)).await?;
+/// // or parse a Query from a string
+/// let query: SingleQuery = "bafyrmieej3j3sprtnbfziv6vhixzr3xxrcabnma43ajb5grhsixdvxzdvu:31".parse().unwrap();
+/// let thirty_first = some_resolver.resolve(query).await?;
+/// // resolve a stitch
+/// let linked = thirty_first.cross_stitches();
+/// let some_other_twine = some_resolver.resolve(linked.stitches().get(0).unwrap().clone()).await?;
+/// # Ok::<_, ResolutionError>(())
+/// # });
+/// ```
 #[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
 pub trait Resolver: BaseResolver {
+  /// Resolve a Twine object from a query
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// # use twine_lib::{resolver::{Resolver, SingleQuery}, errors::ResolutionError, Cid};
+  /// # let strand_cid = Cid::default();
+  /// use twine_lib::store::MemoryStore;
+  /// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+  /// let some_storage = MemoryStore::default();
+  /// // ...
+  /// let thirty_first = some_storage.resolve((strand_cid, 31)).await?;
+  /// # Ok::<_, ResolutionError>(())
+  /// # });
+  /// ```
   async fn resolve<Q: Into<SingleQuery> + MaybeSend>(
     &self,
     query: Q,
@@ -49,6 +107,7 @@ pub trait Resolver: BaseResolver {
     }
   }
 
+  /// Check if a Twine is available for a given query
   async fn has<Q: Into<SingleQuery> + MaybeSend>(&self, query: Q) -> Result<bool, ResolutionError> {
     let query = query.into();
     match query {
@@ -79,6 +138,7 @@ pub trait Resolver: BaseResolver {
     }
   }
 
+  /// Resolve the Twine data with the highest index for a given Strand
   async fn resolve_latest<C: AsCid + MaybeSend>(
     &self,
     strand: C,
@@ -92,6 +152,7 @@ pub trait Resolver: BaseResolver {
     )
   }
 
+  /// Resolve a Twine object by its index on a Strand
   async fn resolve_index<C: AsCid + MaybeSend>(
     &self,
     strand: C,
@@ -109,6 +170,21 @@ pub trait Resolver: BaseResolver {
     )
   }
 
+  /// Resolve a Twine object by a Stitch (a Strand CID and a Tixel CID)
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// # use twine_lib::{resolver::{Resolver, SingleQuery}, errors::ResolutionError, Cid};
+  /// tokio::runtime::Runtime::new().unwrap().block_on(async {
+  /// # use twine_lib::store::MemoryStore;
+  /// # let resolver = MemoryStore::default();
+  /// let strand_cid: Cid = "bafyrmieej3j3sprtnbfziv6vhixzr3xxrcabnma43ajb5grhsixdvxzdvu".parse().unwrap();
+  /// let tixel_cid: Cid = "bafyriqgafbhhudahpnzrdvuzjjczro43i4mnv7637vq4oh6m6lfdccpazmmurfu4vluy7iddrhwbbfvjs62uo2wrzx4axaxx5lv7pfmqveqt2".parse().unwrap();
+  /// let twine = resolver.resolve_stitch(strand_cid, tixel_cid).await?;
+  /// Ok::<_, ResolutionError>(())
+  /// # });
+  /// ```
   async fn resolve_stitch<C: AsCid + MaybeSend>(
     &self,
     strand: C,
@@ -127,6 +203,20 @@ pub trait Resolver: BaseResolver {
     )
   }
 
+  /// Resolve a Strand object by its CID
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// # use twine_lib::{resolver::{Resolver, SingleQuery}, errors::ResolutionError, Cid};
+  /// tokio::runtime::Runtime::new().unwrap().block_on(async {
+  /// # use twine_lib::store::MemoryStore;
+  /// # let resolver = MemoryStore::default();
+  /// let cid: Cid = "bafyrmieej3j3sprtnbfziv6vhixzr3xxrcabnma43ajb5grhsixdvxzdvu".parse().unwrap();
+  /// let latest = resolver.resolve_latest(cid).await?;
+  /// # Ok::<_, ResolutionError>(())
+  /// # });
+  /// ```
   async fn resolve_strand<C: AsCid + MaybeSend>(
     &self,
     strand: C,
@@ -135,6 +225,26 @@ pub trait Resolver: BaseResolver {
     StrandResolution::try_new(*strand_cid, self.fetch_strand(strand_cid).await?)
   }
 
+  /// Resolve a range of Twine objects on a Strand
+  ///
+  /// This can be supplied as a RangeQuery or any type that implements Into<RangeQuery>
+  ///
+  /// # Example
+  ///
+  /// ```no_run
+  /// # use twine_lib::{twine::Twine, resolver::{Resolver, RangeQuery}, errors::ResolutionError, Cid};
+  /// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+  /// # use twine_lib::store::MemoryStore;
+  /// # let resolver = MemoryStore::default();
+  /// let cid_strand: Cid = "bafyrmieej3j3sprtnbfziv6vhixzr3xxrcabnma43ajb5grhsixdvxzdvu".parse().unwrap();
+  /// // resolve the first 11 (because ranges are inclusive) Tixels on the Strand
+  /// let stream = resolver.resolve_range((cid_strand, 0, 10)).await?;
+  /// // dump them into a Vec
+  /// use futures::stream::TryStreamExt;
+  /// let records: Vec<Twine> = stream.try_collect().await?;
+  /// # Ok::<_, ResolutionError>(())
+  /// # });
+  /// ```
   async fn resolve_range<'a, R: Into<RangeQuery> + MaybeSend>(
     &'a self,
     range: R,
@@ -204,10 +314,12 @@ pub trait Resolver: BaseResolver {
     }
   }
 
+  /// Get a stream of all available Strand objects
   async fn strands<'a>(&'a self) -> Result<TwineStream<'a, Strand>, ResolutionError> {
     self.fetch_strands().await
   }
 
+  /// Get the latest index of a Strand
   async fn latest_index(&self, strand: &Cid) -> Result<u64, ResolutionError> {
     Ok(self.fetch_latest(strand).await?.index())
   }
@@ -261,6 +373,24 @@ where
 
 impl<T> Resolver for T where T: AsRef<dyn BaseResolver> + BaseResolverBounds {}
 
+/// A set of resolvers that are tried in series until one succeeds
+///
+/// # Example
+///
+/// ```no_run
+/// # use twine_lib::{resolver::{Resolver, ResolverSetSeries}, errors::ResolutionError, Cid};
+/// # tokio::runtime::Runtime::new().unwrap().block_on(async {
+/// # use twine_lib::store::MemoryStore;
+/// use futures::stream::TryStreamExt;
+/// # let resolver1 = MemoryStore::default();
+/// # let resolver2 = MemoryStore::default();
+/// # let resolver3 = MemoryStore::default();
+/// let mut resolver = ResolverSetSeries::new_boxed(vec![resolver1, resolver2]);
+/// resolver.add_boxed(resolver3);
+/// let strands: Vec<_> = resolver.strands().await?.try_collect().await?;
+/// # Ok::<_, ResolutionError>(())
+/// # });
+/// ```
 #[derive(Clone)]
 pub struct ResolverSetSeries<T>(Vec<T>)
 where
@@ -270,16 +400,19 @@ impl<T> ResolverSetSeries<T>
 where
   T: BaseResolver,
 {
+  /// Create a new ResolverSetSeries from a Vec
   pub fn new(resolvers: Vec<T>) -> Self {
     Self(resolvers)
   }
 
+  /// Add a new resolver to the series
   pub fn add(&mut self, resolver: T) {
     self.0.push(resolver);
   }
 }
 
 impl ResolverSetSeries<Box<dyn BaseResolver>> {
+  /// Create a new ResolverSetSeries of [`Box`]ed resolvers from a Vec
   pub fn new_boxed<T: BaseResolver + 'static>(resolvers: Vec<T>) -> Self {
     Self(
       resolvers
@@ -289,6 +422,7 @@ impl ResolverSetSeries<Box<dyn BaseResolver>> {
     )
   }
 
+  /// Add a new resolver to the series by boxing it
   pub fn add_boxed<T: BaseResolver + 'static>(&mut self, resolver: T) {
     self.add(Box::new(resolver));
   }
